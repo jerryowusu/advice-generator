5345ea3e47db81ffd74622c4d42b928c9b89c7cb
jerryowusu <jerryowusu50@gmail.com>
2023-03-20 22:41:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const api_url = "https://api.adviceslip.com/advice"
+
+async function getApi(url) {
+    const response = await fetch(url);
+
+    let data = await response.json();
+    console.log(data)
+
+    if (response) {
+        hideLoader();
+    }
+
+    show(data)
+}
+
+getApi(api_url)
+
+function hideLoader() {
+    document.getElementById("loading").style.display = 'none';
+}
+
+
+function show(data) {
+
+    let text = `
+    <div class='advice-title'>
+     ADVICE # <span class="title">${data.slip.id}</span>
+    </div>
+    <p class="text">"${data.slip.advice}"</p>
+    `
+     
+    document.getElementById("text").innerHTML = text
+
+}
+
+const reloadButton = document.getElementById('reload-button')
+reloadButton.addEventListener('click', loadAdvice)
+
+function loadAdvice() {
+    window.location.reload()
+    console.log('loading')
+}
